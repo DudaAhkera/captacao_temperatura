1a066463c11be3d27c8a9652560dc5854c83c00b
DudaAhkera <rochapereira16@gmail.com>
2024-10-29 23:18:22 -0300

    Modificacão da estrutura para correcão de erros

--- Captador_de_temperatura.py
+++ Captador_de_temperatura.py
@@ -6,11 +6,26 @@ from selenium.webdriver.support import expected_conditions as EC
 from openpyxl import load_workbook
 import openpyxl
 from tkinter import *
-import mysql.connector
 import chromedriver_autoinstaller
+import pandas as pd
 
+# Definir o caminho para o chromedriver
+chromedriver_autoinstaller.install()
+    
+driver = webdriver.Chrome()
+
+#buscar elemento data/hora
+dt = driver.find_element(By.XPATH, '//*[@id="wob_dts"]').get_attribute('value')
+#buscarelemento temperatura
+temp = driver.find_element(By.XPATH, '//*[@id="wob_tm"]').get_attribute('value')
+#buscar elemento umidade
+umi = driver.find_element(By.XPATH, '//*[@id="wob_wc"]/div[1]/div[2]/div[2]').get_attribute('value')
+
+arquivo = 'Dados_clima.xlsx'
+planilha_nome = 'Lista'
 
 class Aplicacao:
+    
     def __init__(self):
         
         #criacão da janela na interface
@@ -20,7 +35,7 @@ class Aplicacao:
         self.tela = Frame(self.layout)
         self.descricao = Label(self.tela, text="Para gerar arquivos em formato .csv")
         
-        #vincular o botão à funcão que cria a tabela e insere os dados
+        #vincular o botão à funcão que cria arquivo e insere dados
         self.exportar = Button(self.tela, text="Clique aqui", command=self.executar)
         
         #alocacão dos elementos na tela
@@ -33,11 +48,7 @@ class Aplicacao:
         
     # para realizar a captacão das informacões no site com o selenium
     def importar(self):
-        # Definir o caminho para o chromedriver
-        chromedriver_autoinstaller.install()
-        
-        driver = webdriver.Chrome()
-        
+
         #navegar até o site
         driver.get('https://www.google.com.br/')
         
@@ -47,34 +58,29 @@ class Aplicacao:
         #clicar no botão de pesquisar
         driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]').send_keys(Keys.ENTER)
         
-        #buscar elemento data/hora
-        dt = driver.find_element(By.XPATH, '//*[@id="wob_dts"]').get_attribute('value')
-        #buscarelemento temperatura
-        temp = driver.find_element(By.XPATH, '//*[@id="wob_tm"]').get_attribute('value')
-        #buscar elemento umidade
-        umi = driver.find_element(By.XPATH, '//*[@id="wob_wc"]/div[1]/div[2]/div[2]').get_attribute('value')
-        
-        
         print(f"Hoje {dt} a temperatura está {temp} e a umidade do ar é de {umi}%!")
+
+        #fechar o navegador
+        driver.quit()
+            
+    def executar(self):
+        # executar codigo para o botão funcionar
+        tbl = load_workbook(arquivo)
         
-        arquivo = load_workbook('Dados_clima.xlsx')
-        
-        pln = arquivo['lista']
+        pln = tbl[planilha_nome]
         
         pln.cell(row=2, column=1).value = dt
         
         pln.cell(row=2, column=2).value = temp
         
         pln.cell(row=2, column=3).value = umi
-
-        #fechar o navegador
-        driver.quit()
         
-        #return dados
-        arquivo.save('Dados_clima.xlsx')
-        
-        print('Planilha atualizada com sucesso!')
+#return dados
+arquivo.save()    
     
+print('Planilha atualizada com sucesso!')    
+        
+
         
 tl = Aplicacao()
 
